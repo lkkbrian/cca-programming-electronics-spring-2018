870dfade96c487351eaf24e64ed82580ac3b306a
cantor10000 <33783358+cantor10000@users.noreply.github.com>
2018-02-19 17:51:01 -0800

    Update circles-arrays.js

--- class_code/circles-arrays.js
+++ class_code/circles-arrays.js
@@ -3,11 +3,13 @@ var y = [];
 var xSpeed = [];
 var ySpeed = [];
 var colors = [];
+var numberDots;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(300+random(100), 300+random(100));
+  numberDots = 70 + random(30);
 
-  for (var index = 0; index < 100; index = index + 1) {
+  for (var index = 0; index < numberDots; index = index + 1) {
     x[index] = width / 2;
     y[index] = height / 2;
     xSpeed[index] = random(-5, 5);
@@ -20,7 +22,7 @@ function draw() {
   background(0);
   noStroke();
 
-  for (var index = 0; index < 100; index = index + 1) {
+  for (var index = 0; index < numberDots; index = index + 1) {
     fill(colors[index]);
     ellipse(x[index], y[index], 10);
     x[index] = x[index] + xSpeed[index];
